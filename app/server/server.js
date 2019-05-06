c1a8837390df18f2641ccd70bcbe095c72ed171a
Willem HOUM <212682057@ge.com>
2019-05-06 11:43:11 +0200

    starting k8s deployement

--- app/server/server.js
+++ app/server/server.js
@@ -6,6 +6,10 @@ const express = require("express"),
 
 app.use(teaApi)
 
+app.get("hello", (req, res) => {
+  res.send("Hello from Tea API").end();
+})
+
 mongoHelper._initMongoDBConnection(() => {
   app.listen(process.env.APP_PORT, () => {
     console.log("Server listening on port ", process.env.APP_PORT);
